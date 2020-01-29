f881308852cabc2a51931206f01cb835a22d6f1b
Romnet <romnet007@gmail.com>
2020-01-30 02:10:39 +0500

    Initial Commit

---
+++ HW2.1.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.1.1
+//
+//  Created by Роман on 29.01.2020.
+//  Copyright © 2020 Romnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
